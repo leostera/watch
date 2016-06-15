39419f190fd93676ffc5b3754c66568695ca87ee
Leandro Ostera <leandro@ostera.io>
2016-06-15 15:22:05 +0200

    Run go fmt

--- main.go
+++ main.go
@@ -1,162 +1,166 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "os"
-  "os/exec"
-  "strings"
-  "strconv"
-  "syscall"
-  "time"
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
 )
 
-const VERSION="v0.0.1"
+const VERSION = "v0.0.1"
 
 func main() {
-  var i string
-  flag.StringVar(&i, "i", "1s", "Interval to wait between executions in seconds")
-  flag.StringVar(&i, "interval", "1s", "Interval to wait between executions in seconds")
+	var i string
+	flag.StringVar(&i, "i", "1s", "Interval to wait between executions in seconds")
+	flag.StringVar(&i, "interval", "1s", "Interval to wait between executions in seconds")
 
-  var interrupt bool
-  flag.BoolVar(&interrupt, "x", false, "Exit and elevate status code if the command fails")
-  flag.BoolVar(&interrupt, "exit", false, "Exit and elevate status code if the command fails")
+	var interrupt bool
+	flag.BoolVar(&interrupt, "x", false, "Exit and elevate status code if the command fails")
+	flag.BoolVar(&interrupt, "exit", false, "Exit and elevate status code if the command fails")
 
-  var version bool
-  flag.BoolVar(&version, "v", false, "Print out version")
-  flag.BoolVar(&version, "version", false, "Print out version")
+	var version bool
+	flag.BoolVar(&version, "v", false, "Print out version")
+	flag.BoolVar(&version, "version", false, "Print out version")
 
-  var usage bool
-  flag.BoolVar(&usage, "h", false, "Print out version")
-  flag.BoolVar(&usage, "help", false, "Print out version")
+	var usage bool
+	flag.BoolVar(&usage, "h", false, "Print out version")
+	flag.BoolVar(&usage, "help", false, "Print out version")
 
-  flag.Parse()
+	flag.Parse()
 
-  if version {
-    die(0, VERSION)
-  }
+	if version {
+		die(0, VERSION)
+	}
 
-  command := flag.Args()
+	command := flag.Args()
 
-  if usage || len(command) == 0 {
-    help()
-    die(0, "")
-  }
+	if usage || len(command) == 0 {
+		help()
+		die(0, "")
+	}
 
-  if ! commandExists(command) {
-    die(0, "Executable not found in PATH")
-  }
+	if !commandExists(command) {
+		die(0, "Executable not found in PATH")
+	}
 
-  interval := parseInterval(i)
+	interval := parseInterval(i)
 
-  loop(intervalToTime(interval), func () {
-    reset()
-    status := run(command)
-    if(interrupt && status != 0) {
-      die(status, "")
-    }
-  })
+	loop(intervalToTime(interval), func() {
+		reset()
+		status := run(command)
+		if interrupt && status != 0 {
+			die(status, "")
+		}
+	})
 }
 
 func intervalSuffixes() []string {
-  return []string{ "ms", "s", "MS", "S" }
+	return []string{"ms", "s", "MS", "S"}
 }
 
 func unitToFactor(s string) float64 {
-  if s == "ms" || s == "MS" { return 1.0 }
-  if s == "S" || s == "S" { return 1000.0 }
-  return 1000.0
+	if s == "ms" || s == "MS" {
+		return 1.0
+	}
+	if s == "S" || s == "S" {
+		return 1000.0
+	}
+	return 1000.0
 }
 
-func suffixToInterval (s string, i string) (float64, bool) {
-  is_unit := strings.HasSuffix(i, s)
-  if is_unit {
-    n, err := strconv.ParseFloat(i[:len(i)-len(s)], 64)
-    dieIf(err, -1, "Invalid interval")
-    return n*unitToFactor(s), true
-  }
-  return 0, false
+func suffixToInterval(s string, i string) (float64, bool) {
+	is_unit := strings.HasSuffix(i, s)
+	if is_unit {
+		n, err := strconv.ParseFloat(i[:len(i)-len(s)], 64)
+		dieIf(err, -1, "Invalid interval")
+		return n * unitToFactor(s), true
+	}
+	return 0, false
 }
 
 func parseInterval(i string) float64 {
-  suffixes := intervalSuffixes()
-  for j := 0; j < len(suffixes); j++ {
-    if val, is := suffixToInterval(suffixes[j], i); is  {
-      return val
-    }
-  }
-  return 0
+	suffixes := intervalSuffixes()
+	for j := 0; j < len(suffixes); j++ {
+		if val, is := suffixToInterval(suffixes[j], i); is {
+			return val
+		}
+	}
+	return 0
 }
 
 func help() {
-  br := func () { fmt.Println("") }
-  _p  := func (p string, t string) { fmt.Println(p, t) }
-  p := func(t string) { _p("  ", t) }
-  pp := func(t string) { _p("    ", t) }
+	br := func() { fmt.Println("") }
+	_p := func(p string, t string) { fmt.Println(p, t) }
+	p := func(t string) { _p("  ", t) }
+	pp := func(t string) { _p("    ", t) }
 
-  br()
-  p("Usage: watch [options] <cmd>")
-  br()
-  p("Sample: watch -i=100ms make")
-  br()
-  p("Options:")
-  br()
-  pp("-i, --interval\t\tinterval in seconds or ms, defaulting to 1s")
-  pp("-x, --exit\t\t\texit on failure")
-  pp("-v, --version\t\tprint out version")
-  pp("-h, --help\t\t\tthis help page")
-  br()
+	br()
+	p("Usage: watch [options] <cmd>")
+	br()
+	p("Sample: watch -i=100ms make")
+	br()
+	p("Options:")
+	br()
+	pp("-i, --interval\t\tinterval in seconds or ms, defaulting to 1s")
+	pp("-x, --exit\t\t\texit on failure")
+	pp("-v, --version\t\tprint out version")
+	pp("-h, --help\t\t\tthis help page")
+	br()
 }
 
 func dieIf(err error, status int, message string) {
-  if err != nil {
-    die(status, message)
-  }
+	if err != nil {
+		die(status, message)
+	}
 }
 
 func die(status int, message string) {
-  if len(message) > 0 {
-    fmt.Println(message)
-  }
-  os.Exit(status)
+	if len(message) > 0 {
+		fmt.Println(message)
+	}
+	os.Exit(status)
 }
 
 func intervalToTime(i float64) time.Duration {
-  return time.Duration(i) * time.Millisecond
+	return time.Duration(i) * time.Millisecond
 }
 
 func reset() {
-  run([]string {"clear"})
+	run([]string{"clear"})
 }
 
 func makeCmd(bin string, args []string) *exec.Cmd {
-  cmd := exec.Command(bin, args...)
-  cmd.Env = os.Environ()
-  cmd.Stdout = os.Stdout
-  cmd.Stderr = os.Stderr
-  return cmd
+	cmd := exec.Command(bin, args...)
+	cmd.Env = os.Environ()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd
 }
 
 func getExitStatus(cmd *exec.Cmd) int {
-  status, _ := cmd.ProcessState.Sys().(syscall.WaitStatus)
-  return status.ExitStatus()
+	status, _ := cmd.ProcessState.Sys().(syscall.WaitStatus)
+	return status.ExitStatus()
 }
 
 func commandExists(command []string) bool {
-  _, result := exec.LookPath(command[0])
-  return result == nil
+	_, result := exec.LookPath(command[0])
+	return result == nil
 }
 
 func run(command []string) int {
-  path, _ := exec.LookPath(command[0])
-  cmd := makeCmd(path, command[1:])
-  cmd.Run()
-  return getExitStatus(cmd)
+	path, _ := exec.LookPath(command[0])
+	cmd := makeCmd(path, command[1:])
+	cmd.Run()
+	return getExitStatus(cmd)
 }
 
 func loop(d time.Duration, fn func()) {
-  fn()
-  for range time.Tick(d) {
-    fn()
-  }
+	fn()
+	for range time.Tick(d) {
+		fn()
+	}
 }
--- watch_test.go
+++ watch_test.go
@@ -1,19 +1,19 @@
 package main
 
-import(
-  "testing"
+import (
+	"testing"
 )
 
 /******************************************************************************/
 // Fixtures
 /******************************************************************************/
 
-func fixtureCmd() string { return "sh" }
-func fixtureArgs() []string { return []string { "-c", "exit 0" } }
-func fixtureBadArgs() []string { return []string { "-c", "exit 2"} }
+func fixtureCmd() string       { return "sh" }
+func fixtureArgs() []string    { return []string{"-c", "exit 0"} }
+func fixtureBadArgs() []string { return []string{"-c", "exit 2"} }
 
 func fixtureCmdSlice(args []string) []string {
-  return append( []string { fixtureCmd() }, args...)
+	return append([]string{fixtureCmd()}, args...)
 }
 
 /******************************************************************************/
@@ -21,74 +21,73 @@ func fixtureCmdSlice(args []string) []string {
 /******************************************************************************/
 
 func TestSuffixToInterval1MS(t *testing.T) {
-  val, _ := suffixToInterval("MS", "1MS")
-  if val != 1 {
-    t.Fatalf("%s should be 1", val)
-  }
+	val, _ := suffixToInterval("MS", "1MS")
+	if val != 1 {
+		t.Fatalf("%s should be 1", val)
+	}
 }
 
 func TestSuffixToInterval1ms(t *testing.T) {
-  val, _ := suffixToInterval("ms", "1ms")
-  if val != 1 {
-    t.Fatalf("%s should be 1", val)
-  }
+	val, _ := suffixToInterval("ms", "1ms")
+	if val != 1 {
+		t.Fatalf("%s should be 1", val)
+	}
 }
 
 func TestSuffixToInterval1S(t *testing.T) {
-  val, _ := suffixToInterval("S", "1S")
-  if val != 1000.0 {
-    t.Fatalf("%s should be 100.0", val)
-  }
+	val, _ := suffixToInterval("S", "1S")
+	if val != 1000.0 {
+		t.Fatalf("%s should be 100.0", val)
+	}
 }
 
 func TestSuffixToInterval1s(t *testing.T) {
-  val, _ := suffixToInterval("s", "1s")
-  if val != 1000.0 {
-    t.Fatalf("%s should be 1000.0", val)
-  }
+	val, _ := suffixToInterval("s", "1s")
+	if val != 1000.0 {
+		t.Fatalf("%s should be 1000.0", val)
+	}
 }
 
 func TestRunSuccessfully(t *testing.T) {
-  ok := run(fixtureCmdSlice(fixtureArgs()))
-  if ok != 0 {
-    t.Fatalf("%s should be 0", ok)
-  }
+	ok := run(fixtureCmdSlice(fixtureArgs()))
+	if ok != 0 {
+		t.Fatalf("%s should be 0", ok)
+	}
 }
 
 func TestRunExit(t *testing.T) {
-  err := run(fixtureCmdSlice(fixtureBadArgs()))
-  if err != 2 {
-    t.Fatalf("%s should not be 2", err)
-  }
+	err := run(fixtureCmdSlice(fixtureBadArgs()))
+	if err != 2 {
+		t.Fatalf("%s should not be 2", err)
+	}
 }
-
 
 /******************************************************************************/
 // Benchmarks
 /******************************************************************************/
 
 func BenchmarkRunSuccessfully(b *testing.B) {
-  cmd := fixtureCmdSlice(fixtureArgs())
-  for n := 0; n < b.N; n++ {
-    run(cmd)
-  }
+	cmd := fixtureCmdSlice(fixtureArgs())
+	for n := 0; n < b.N; n++ {
+		run(cmd)
+	}
 }
 
 func BenchmarkRunExit(b *testing.B) {
-  cmd := fixtureCmdSlice(fixtureBadArgs())
-  for n := 0; n < b.N; n++ {
-    run(cmd)
-  }
+	cmd := fixtureCmdSlice(fixtureBadArgs())
+	for n := 0; n < b.N; n++ {
+		run(cmd)
+	}
 }
 
 func BenchmarkIntervalToTime(b *testing.B) {
-  for n := 0; n < b.N; n++ {
-    intervalToTime(1)
-  }
+	for n := 0; n < b.N; n++ {
+		intervalToTime(1)
+	}
 }
 
 func BenchmarkSuffixToInterval(b *testing.B) {
-  for n := 0; n < b.N; n++ {
-    suffixToInterval("MS", "1MS")
-  }
+	for n := 0; n < b.N; n++ {
+		suffixToInterval("MS", "1MS")
+	}
 }
